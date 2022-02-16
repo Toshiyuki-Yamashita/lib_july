2b349a5b0b8525959c54cbaf8c0bcbf45a8eaa9b
Toshiyuki-Yamashita <Toshiyuki-Yamashita@users.noreply.github.com>
2022-02-16 20:58:12 +0900

    [refactor] grouping the test

--- spec/string/case_spec.rb
+++ spec/string/case_spec.rb
@@ -1,17 +1,37 @@
 # frozen_string_literal: true
 
 require "string/case"
-RSpec.describe Case do
+RSpec.describe String do
   using Case
-  it "is nil" do
-    expect("".case).to eq nil
-  end
-
-  it "is match /a/" do
-    expect("abcde".case.when(/a/) {_2.to_s}).to eq "a"
-  end
-
-  it "is match /a/ or /b/" do
-    expect("abcde".case.when(/a/, /b/) {_2.to_s}).to eq "a"
+  describe "#case" do
+    describe "not matched" do
+      context "only case" do
+        it "is nil" do
+          expect("".case).to eq nil
+        end
+      end
+      context "without else" do
+        it "is nil" do
+          expect("".case.when(/a/){_2.to_s}).to eq nil
+        end
+      end
+    end
+    describe "matched to 'abcde'" do
+      subject { str.case.when(*regexp){_2.to_s} }
+      context "#when /a/" do
+        let(:str) { "abcde" }
+        let(:regexp) { [/a/] }
+        it "is 'a'" do
+          is_expected.to eq "a"
+        end
+      end
+      context "#when /a/,/b/" do
+        let(:str) { "abcde" }
+        let(:regexp) { [/a/, /b/] }
+        it "is 'a'" do
+          expect("abcde".case.when(/a/, /b/) {_2.to_s}).to eq "a"
+        end
+      end
+    end
   end
 end
