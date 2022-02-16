b75fa25f315041deaf952cf75a8bbe5b8b0ce1a7
Toshiyuki-Yamashita <Toshiyuki-Yamashita@users.noreply.github.com>
2022-02-16 22:25:54 +0900

    rubocop rspec
    * add rubocop-rake
    * fix rspec offences

--- spec/string/case_spec.rb
+++ spec/string/case_spec.rb
@@ -1,35 +1,41 @@
 # frozen_string_literal: true
 
 require "string/case"
-RSpec.describe String do
-  using Case
-  describe "#case" do
+RSpec.describe Case do
+  using described_class
+  describe "String#case" do
     describe "not matched" do
-      context "only case" do
+      context "when only case" do
         it "is nil" do
           expect("".case).to eq nil
         end
       end
+
       context "without else" do
         it "is nil" do
           expect("".case.when(/a/){_2.to_s}).to eq nil
         end
       end
     end
+
     describe "matched to 'abcde'" do
-      subject { str.case.when(*regexp){_2.to_s} }
-      context "#when /a/" do
+      subject(:with) { str.case.when(*regexp){_2.to_s} }
+
+      describe "#when /a/" do
         let(:str) { "abcde" }
         let(:regexp) { [/a/] }
+
         it "is 'a'" do
-          is_expected.to eq "a"
+          expect(with).to eq "a"
         end
       end
-      context "#when /a/,/b/" do
+
+      describe "#when /a/,/b/" do
         let(:str) { "abcde" }
         let(:regexp) { [/a/, /b/] }
+
         it "is 'a'" do
-          expect("abcde".case.when(/a/, /b/) {_2.to_s}).to eq "a"
+          expect(with).to eq "a"
         end
       end
     end
